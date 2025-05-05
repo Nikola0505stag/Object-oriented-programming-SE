a746038666dbe5bc5c63236f6e998144b20decb4
Nikola Neychev <60015433+Nikola0505stag@users.noreply.github.com>
2025-05-05 19:44:31 +0300

    Delete Ticket directory

--- Ticket/Ticket/GroupTicket.cpp
+++
@@ -1,7 +0,0 @@
-#include "GroupTicket.h"
-
-GroupTicket::GroupTicket():Ticket()
-{}
-
-GroupTicket::GroupTicket(const char* name, double price):Ticket(name,price*0.25)
-{}
--- Ticket/Ticket/StudentTicket.cpp
+++
@@ -1,8 +0,0 @@
-#include "StudentTicket.h"
-
-StudentTicket::StudentTicket(): Ticket()
-{}
-
-StudentTicket::StudentTicket(const char* name, double price):Ticket(name,price / 2)
-{
-}
