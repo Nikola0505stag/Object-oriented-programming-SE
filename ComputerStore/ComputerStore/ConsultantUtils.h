b9ae7adc18d3bfe10ba47724d2ff79591fc2fd91
Nikola Neychev <60015433+Nikola0505stag@users.noreply.github.com>
2025-05-05 19:44:54 +0300

    Delete ComputerStore directory

--- ComputerStore/ComputerStore/ConsultantUtils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Computer.h"
-
-class ConsultantUtils {
-public:
-    static bool isGoodForGaming(const Computer& computer);
-    static bool isGoodForTravel(const Computer& computer);
-};
-
